38c4d157d968df48c3751d9411e4d37a942acf5d
KUNAL SINGH <kunal.exuberant@gmail.com>
2015-07-12 06:57:39 +0530

    changes in server

--- server.js
+++ server.js
@@ -4,15 +4,16 @@
 
 var http_server = require("http");
 var mongo = require('mongodb');
+var express = require('express');
+var router = express.Router();
+var app = express();
+
+var data;
+
+
 
 http_server.createServer(function(request, response){
     response.writeHead(200);
-
-    var data = {
-
-        "user1":"Kunal singh",  "user2":"Ankit singh", "User 3" : "Ajit Singh", "user4": "Rohit Saxena"
-
-    };
 
     //response.write("This is the expense manager"+ data.toString());
 
@@ -21,57 +22,115 @@ http_server.createServer(function(request, response){
     var MongoClient = mongodb.MongoClient;
     var url = 'mongodb://localhost:27017/expense_manager';
 
-    MongoClient.connect(url, function (err, db) {
-        if (err) {
-            console.log('Unable to connect to the mongoDB server. Error:', err);
-        } else {
-            console.log('Connection established to', url);
+    console.log(request.url);
+    if(request.url.indexOf('/transaction_new?expenditure') > -1) {
 
+            console.log("new transaction");
+        MongoClient.connect(url, function (err, db) {
+            if (err) {
+                console.log('Unable to connect to the mongoDB server. Error:', err);
+            } else {
 
-            var collection = db.collection('users');
+                console.log(decodeURIComponent(request.url.substring(request.url.indexOf("expenditure=") + 12)));
 
-            //Create some users
-            var user1 = {name: 'modulus admin', age: 42, roles: ['admin', 'moderator', 'user']};
-            var user2 = {name: 'modulus user', age: 22, roles: ['user']};
-            var user3 = {name: 'modulus super admin', age: 92, roles: ['super-admin', 'admin', 'moderator', 'user']};
+                var expenditure = decodeURIComponent(request.url.substring(request.url.indexOf("expenditure=") + 12));
 
-            // Insert some users
-            collection.insert([user1, user2, user3], function (err, result) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log('Inserted %d documents into the "users" collection. The documents inserted with "_id" are:', result.length, result);
+                expenditure = JSON.parse(expenditure);
+
+                var collection = db.collection('transaction');
+                expenditure["transaction_id"] = Math.random()* 1000000000000000000;
+                expenditure['logged_by'] = "kunal.exuberant@gmail.com";
+                // Insert some users
+                collection.insert(expenditure, function (err, result) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        //console.log('Inserted %d documents into the "users" collection. The documents inserted with "_id" are:', result.length, result);
+                    }
+                    db.close();
+                });
+
+            }
+        });
+
+        data = {
+            "friends": [
+                {
+                    "name": "Kunal Singh",
+                    "userid": "1905",
+                    "balance": "-1200"
+                },
+                {
+                    "name": "Ankit Singh",
+                    "userid": "1925",
+                    "balance": "-1240"
+                },
+                {
+                    "name": "Ajit Singh",
+                    "userid": "1944",
+                    "balance": "1200"
+                },
+                {
+                    "name": "Rohit Saxena",
+                    "userid": "1205",
+                    "balance": "-1230"
                 }
+            ]
+        };
+    }
+    else {
+
+        console.log("old transaction");
+        MongoClient.connect(url, function (err, db) {
+            if (err) {
+                console.log('Unable to connect to the mongoDB server. Error:', err);
+            } else {
+                //console.log('Connection established to', url);
+
+                var collection = db.collection('transaction');
+
+                collection.find({logged_by: 'kunal.exuberant@gmail.com'}).toArray(function (err, result) {
+                    if (err) {
+                        console.log(err);
+                    } else if (result.length) {
+                        console.log('Found:', result);
+                        data = result;
+                    } else {
+                        console.log('No document(s) found with defined "find" criteria!');
+                    }
+                });
                 db.close();
-            });
-        }
-    });
+            }
+        });
 
+        //response.setHeader('Content-Type', 'application/json');
+        // response.send();
 
-    MongoClient.connect(url, function (err, db) {
-        if (err) {
-            console.log('Unable to connect to the mongoDB server. Error:', err);
-        } else {
-            console.log('Connection established to', url);
-
-            var collection = db.collection('users');
-
-            collection.find({name: 'modulus user'}).toArray(function (err, result) {
-                if (err) {
-                    console.log(err);
-                } else if (result.length) {
-                    console.log('Found:', result);
-                } else {
-                    console.log('No document(s) found with defined "find" criteria!');
+        data = {
+            "friends": [
+                {
+                    "name": "Ankit Singh",
+                    "userid": "1905",
+                    "balance": "-200"
+                },
+                {
+                    "name": "Kunal Singh",
+                    "userid": "1925",
+                    "balance": "2400"
+                },
+                {
+                    "name": "Ajit Singh",
+                    "userid": "1944",
+                    "balance": "600"
+                },
+                {
+                    "name": "Rohit Saxena",
+                    "userid": "1205",
+                    "balance": "-100"
                 }
-
-            });
-            db.close();
-        }
-    });
-
-    //response.setHeader('Content-Type', 'application/json');
-   // response.send();
+            ]
+        };
+    }
 
     response.end(JSON.stringify(data));
 
